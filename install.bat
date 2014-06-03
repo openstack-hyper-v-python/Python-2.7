241719e1840f2c071290e2c04f51a5eaa7a38b58
gabloe <t-gabloe@microsoft.com>
2014-06-03 13:39:04 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,5 @@
 rmdir /s /q C:\Python27
 %~dps0\PCbuild\python.exe %~dps0\install_python.py
+cd C:\Python27
+gendef.exe python27.dll
+dlltool.exe --dllname python27.dll --def python27.def --output-lib libs\libpython27.a
